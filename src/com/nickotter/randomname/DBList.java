9403d9fb63b4fcb4a88e6c87124aaa5505a96e4d
otternq <otternq@gmail.com>
2013-01-26 19:21:18 -0800

    now when an item on the list is clicked, the device says what is on the list item

--- src/com/nickotter/randomname/DBList.java
+++ src/com/nickotter/randomname/DBList.java
@@ -1,5 +1,7 @@
 package com.nickotter.randomname;
 
+import java.util.Locale;
+
 import android.support.v4.app.ListFragment;
 import android.content.Context;
 import android.os.Bundle;
@@ -9,11 +11,17 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
+import android.speech.tts.TextToSpeech;
 
-public class DBList extends ListFragment {
+public class DBList extends ListFragment implements
+TextToSpeech.OnInitListener {
 
 	final String LOGTAG = "DBList";
+	
+	private TextToSpeech tts;
+	
 	
 	String[] countries = new String[] {
         "India",
@@ -27,6 +35,12 @@ public class DBList extends ListFragment {
         "South Korea",
         "Japan"
     };
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		tts = new TextToSpeech(getActivity(), this);
+	}
 	 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -37,6 +51,49 @@ public class DBList extends ListFragment {
         setListAdapter(adapter);
  
         return super.onCreateView(inflater, container, savedInstanceState);
+    }
+	
+	@Override
+    public void onDestroy() {
+        // Don't forget to shutdown tts!
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
+    }
+	
+	@Override 
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Log.v(LOGTAG, "You clicked on item number " + position);
+        Log.v(LOGTAG, "The selected item is: " + this.countries[position]);
+        speakOut(this.countries[position]);
+    }
+
+	@Override
+    public void onInit(int status) {
+ 
+        if (status == TextToSpeech.SUCCESS) {
+ 
+            int result = tts.setLanguage(Locale.US);
+ 
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "This Language is not supported");
+            } else {
+                speakOut("Hello");
+            }
+ 
+        } else {
+            Log.e("TTS", "Initilization Failed!");
+        }
+ 
+    }
+	
+	
+	private void speakOut(String text) {
+ 
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
     }
 	
 
