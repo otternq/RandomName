fa6a688cb585bb9c8e4417d72cd339f5a9c82d0f
kimquy <nguy1461@vandals.uidaho.edu>
2013-02-09 13:42:30 -0800

    change database

--- src/com/nickotter/randomname/Sqlite.java
+++ src/com/nickotter/randomname/Sqlite.java
@@ -24,27 +24,18 @@ public class Sqlite extends SQLiteOpenHelper {
 	public static final String DATABASE_LIST = "listManager";
 	
 	public static final String ITEM_ID = "id";
-	public static final String ITEM_NAME = "name";
+	public static final String ITEM_NAME = "itemName";
 	public static final String ITEM_LIST_ID = "listID";
 
 	public static final String	GROUP_ID = "id";
-	public static final String GROUP_NAME = "name";
+	public static final String GROUP_NAME = "groupName";
 	
 	public static final String	LIST_ID = "id";
-	public static final String LIST_NAME = "name";
+	public static final String LIST_NAME = "listName";
 	public static final String LIST_GROUP_ID = "groupID";
 	
 	
-	String CREATE_ITEM_TABLE = "CREATE TABLE " + DATABASE_ITEM + "("
-            + ITEM_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + ITEM_NAME + " TEXT," + ")";
-    
-    String CREATE_GROUP_TABLE = "CREATE TABLE " + DATABASE_GROUP + "("
-    		+ GROUP_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + GROUP_NAME + " TEXT," + ")";
-    
-    String CREATE_LIST_TABLE = "CREATE TABLE " + DATABASE_LIST + "("
-    		+ LIST_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," 
-    		+ LIST_GROUP_ID + " INTEGER," 
-    		+ LIST_NAME + " TEXT," + ")";
+	
 	
 	public Sqlite(Context context){
 		super(context, DATABASE_NAME, null, DATABASE_ITEM_VERSION);
@@ -52,6 +43,17 @@ public class Sqlite extends SQLiteOpenHelper {
 
 	@Override
     public void onCreate(SQLiteDatabase db) {
+		
+		String CREATE_ITEM_TABLE = "CREATE TABLE " + DATABASE_ITEM + "("
+	            + ITEM_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + ITEM_NAME + " TEXT" + ")";
+	    
+	    String CREATE_GROUP_TABLE = "CREATE TABLE " + DATABASE_GROUP + "("
+	    		+ GROUP_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + GROUP_NAME + " TEXT" + ")";
+	    
+	    String CREATE_LIST_TABLE = "CREATE TABLE " + DATABASE_LIST + "("
+	    		+ LIST_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," 
+	    		+ LIST_GROUP_ID + " INTEGER," 
+	    		+ LIST_NAME + " TEXT" + ")";
         
         db.execSQL(CREATE_ITEM_TABLE);
         
