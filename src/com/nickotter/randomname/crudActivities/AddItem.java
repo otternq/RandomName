60f8b4801646066f7f16c05c82371ed1beaf004f
otternq <otternq@gmail.com>
2013-03-08 14:49:28 -0800

    crud activities now navigate back with the app/home button

--- src/com/nickotter/randomname/crudActivities/AddItem.java
+++ src/com/nickotter/randomname/crudActivities/AddItem.java
@@ -2,9 +2,11 @@ package com.nickotter.randomname.crudActivities;
 
 import android.os.Bundle;
 
+import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
 import com.nickotter.randomname.R;
 import com.nickotter.randomname.R.layout;
 import com.nickotter.randomname.R.menu;
@@ -15,6 +17,9 @@ public class AddItem extends SherlockFragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.add_group);
+		
+		ActionBar bar = getSupportActionBar();
+        bar.setDisplayHomeAsUpEnabled(true);
 	}
 	
 	@Override
@@ -23,5 +28,17 @@ public class AddItem extends SherlockFragmentActivity {
 	    inflater.inflate(R.menu.edit_general, menu);
 	    return true;
 	}
+	
+	public boolean onOptionsItemSelected(MenuItem item) 
+    {    
+       switch (item.getItemId()) 
+       {        
+          case android.R.id.home:            
+        	  finish();
+              return true;       
+          default:            
+             return super.onOptionsItemSelected(item);    
+       }
+    }
 
 }
