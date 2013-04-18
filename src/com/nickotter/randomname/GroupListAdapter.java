a1e26a8bbaf15e18eb6d2528ea166507dfb4effc
otternq <otternq@gmail.com>
2013-04-18 13:58:07 -0700

    adding edit and delete icons for group list's list view

--- src/com/nickotter/randomname/GroupListAdapter.java
+++ src/com/nickotter/randomname/GroupListAdapter.java
@@ -1,6 +1,8 @@
 package com.nickotter.randomname;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,15 +17,19 @@ import com.nickotter.randomname.Item;
 public class GroupListAdapter extends BaseAdapter {
 	
 	private List<Group> list = null;
-	private Context context = null;
+	private Activity context = null;
+	
+    private static LayoutInflater inflater=null;
 
 	/**
 	 * @param args
 	 */
-	public GroupListAdapter(Context context, List<Group> items) {
+	public GroupListAdapter(Activity context, List<Group> items) {
 		this.list = items;
 		this.context = context;
 
+		
+		inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 	}
 
 	@Override
@@ -43,14 +49,29 @@ public class GroupListAdapter extends BaseAdapter {
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		TextView itemLayout;
-		ImageView image;
+		
+		View vi=convertView;
+        if(convertView==null) {
+            vi = inflater.inflate(R.layout.group_row, null);
+        }
+
         Group item = this.list.get(position);
-
-        itemLayout= (TextView) LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
-        itemLayout.setText(item.getName());
-
-        return itemLayout;
+        
+        TextView text=(TextView)vi.findViewById(R.id.row_name);
+        text.setText(item.getName());
+        
+        int editImageDrawable = R.drawable.content_edit;
+        int deleteImageDrawable = R.drawable.content_discard;
+        
+        ImageView editImage = (ImageView)vi.findViewById(R.id.row_edit);
+        Drawable imageDrawable = this.context.getResources().getDrawable(editImageDrawable);
+        editImage.setImageDrawable(imageDrawable);
+        
+        ImageView deleteImage =(ImageView)vi.findViewById(R.id.row_delete);
+        Drawable imageDrawableDelete = this.context.getResources().getDrawable(deleteImageDrawable);
+        deleteImage.setImageDrawable(imageDrawableDelete);
+        
+        return vi;
 	}
 
 }
